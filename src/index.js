7baf5296675b46e383ed5bdb223f8eef885d382d
Juan Fran Martin <juanframaro@gmail.com>
2021-11-14 10:36:52 +0000

    updated index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+/* const sections = document.querySelectorAll("section");
+const footer = document.getElementsByTagName("footer");
+console.log(footer);
+const navLi = document.querySelectorAll("nav .container ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    const sectionBottom = footer.clientHeight;
+    console.log(sectionTop);
+    console.log(sectionBottom);
+    console.log(sectionHeight);
+    if (scrollY >= (sectionTop - sectionHeight / 3)) {
+      current = section.getAttribute("id");
+      // console.log(current);
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+}); */
+
 const navLinks = document.querySelectorAll("a");
 navLinks.forEach(function(link) {
   link.addEventListener("click", function(e) {
     const href = e.target.getAttribute("href");
     if (href.includes("#")) {
       const attr = href.toString().replace("#", "");
-      console.log(attr);
       const target = document.getElementById(attr);
       target.scrollIntoView({
         behavior: "smooth",

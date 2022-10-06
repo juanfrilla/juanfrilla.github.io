e8b85c27973e287093f9ba5cb411e1d4e6dadc97
Juan Fran Martin <juanframaro@gmail.com>
2022-10-06 19:09:45 +0100

    ammending stuff

--- src/index.js
+++ src/index.js
@@ -34,11 +34,11 @@ const language = async () => {
     "add_training_desc_2", "add_training_desc_3", "add_training_desc_4", "add_training_desc_5", "add_training_desc_6", "add_training_desc_7", "view_project1", "view_project2"]
 
   if (isChecked === true) {
-    forEach(navbar_ids, function (id) {
+    navbar_ids.forEach(function (id) {
       document.getElementById(id).innerHTML = json['en'][id]
     })
   } else {
-    forEach(navbar_ids, function (id) {
+    navbar_ids.forEach(function (id) {
       document.getElementById(id).innerHTML = json['es'][id]
     })
   }

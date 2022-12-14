2f0ddef9d7e44a7e27a13f556a3cf98a5fd5b682
Juan Fran Martin <juanframaro@gmail.com>
2022-12-14 21:38:12 +0000

    removing last part of url path

--- index.js
+++ index.js
@@ -9,23 +9,25 @@ navLinks.forEach(function (link) {
         behavior: "smooth",
         block: "center"
       });
-      history.pushState(null, null, attr);
+      const url = window.location.href
+      history.pushState(null, null, url);
       e.preventDefault();
     }
   });
 });
 
-async function loadJSON() {
+
+const loadJSON = async () => {
   const response = await fetch("./assets/lang.json");
   const json = await response.json();
   return json
+
 }
 
 
-var check = document.querySelector(".check");
-
+var switcher = document.querySelector(".switcher");
 const language = async () => {
-  var isChecked = check.checked;
+  var isSwitched = switcher.checked;
   const json = await loadJSON()
 
   const navbar_ids = ["title", "header", "about_nav", "training_nav", "addtraining_nav", "languages_nav", "experience_nav", "another_nav",
@@ -34,7 +36,7 @@ const language = async () => {
     "experience_1", "experience_2", "experience_3", "experience_4", "personal_1", "personal_2", "add_training_text", "add_training_desc_1",
     "add_training_desc_2", "add_training_desc_3", "add_training_desc_4", "add_training_desc_5", "add_training_desc_6", "add_training_desc_7"]
 
-  if (isChecked === true) {
+  if (isSwitched === true) {
     for (var id = 0; id < navbar_ids.length; id++) {
       document.getElementById(navbar_ids[id]).innerHTML = json['en'][navbar_ids[id]]
     }
@@ -44,23 +46,5 @@ const language = async () => {
     }
   }
 }
-check.addEventListener("click", language);
-
-
-// Si se refresca la página te lleva al inicio
-const pageAccessedByReload = (
-  (window.performance.navigation && window.performance.navigation.type === 1) ||
-  window.performance
-    .getEntriesByType('navigation')
-    .map((nav) => nav.type)
-    .includes('reload')
-);
-const url = window.location.href
-console.log(url)
-if (pageAccessedByReload === true) {
-  const result = url.substring(0, url.lastIndexOf('/'))
-
-  window.location.href = result;
-
-}
+switcher.addEventListener("click", language);
 
